ece9e5a1ee73e688f3f4a5c6469a726ae3d8baaf
franlol <fraaaaan@gmail.com>
2019-08-07 21:38:37 +0200

    Updates

---
+++ precache-manifest.ee76a08a9e5386732cc09ed6b97e5d69.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "783634e09bc629dceb07",
+    "url": "/shopping-cart-vconsoles/static/css/main.9b8ed365.chunk.css"
+  },
+  {
+    "revision": "783634e09bc629dceb07",
+    "url": "/shopping-cart-vconsoles/static/js/main.1c7e27a8.chunk.js"
+  },
+  {
+    "revision": "b3581c87de05b95a29ba",
+    "url": "/shopping-cart-vconsoles/static/js/runtime~main.3d126966.js"
+  },
+  {
+    "revision": "43f383999bcd00fb2c59",
+    "url": "/shopping-cart-vconsoles/static/css/2.009d05f3.chunk.css"
+  },
+  {
+    "revision": "43f383999bcd00fb2c59",
+    "url": "/shopping-cart-vconsoles/static/js/2.120e63bd.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shopping-cart-vconsoles/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shopping-cart-vconsoles/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shopping-cart-vconsoles/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shopping-cart-vconsoles/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shopping-cart-vconsoles/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3456e0ef984b721a11eedefe77376dd5",
+    "url": "/shopping-cart-vconsoles/index.html"
+  }
+];
